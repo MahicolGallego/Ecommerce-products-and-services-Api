0bc787aa1d62ba845cf713a0332ab4e90553a906
Mahi_G13 <maicolgallego14@gmail.com>
2025-01-24 14:43:57 -0500

    feat(config): initial project setup with TypeORM and PostgreSQL connection
    - Preconfiguration for connection to PostgreSQL database
    - Installed dependencies: nest/config, class-validator, class-transformer, bcrypt
    - Implemented global pipe for validation and transformation
    - Enabled CORS
    - Enabled serialization interceptor for class-transformer functionality
    - Implemented centralized error handling (Error manager)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize:
+          configService.get<string>('STAGE') === 'prod' ? false : true, // Automatically synchronize the database schema (development only)
+        logging: false, // Enable logging of SQL queries (development only)
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
+  //enable CORS (Cross-Origin Resource Sharing)
+  app.enableCors();
+
+  // validations and data transformations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  //enable deserialization of response objects (apply class-transformer)
+
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
